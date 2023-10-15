8741fd1dd5d2eecffdc942d01ba9ab38b81aa456
temiloluwa <temireply@gmail.com>
2023-10-16 01:23:52 +0200

    changed location of aiflow

---
+++ aiflow/README.md
@@ -0,0 +1,11 @@
+# AIflow Library
+
+AIflow is a very lightweight prompting Python library
+
+## Installation
+
+You can install AIflow using pip:
+
+```
+pip install aiflow 
+```
---
+++ aiflow/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="aiflow",
+    version="0.1.0",
+    description="A lightweight prompting library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="temiloluwa adeoti",
+    author_email="temilolu74@gmail.com",
+    url="https://github.com/temiloluwa/lingua_trainer/aiflow",
+    packages=find_packages(),
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.9",
+    ],
+    keywords="lightweight llm prompting library",
+    install_requires=[
+        "openai"
+    ],
+    python_requires=">=3.9",
+)
