470dafc022caf51507178e7702f030a3d8624a8d
Temiloluwa <temilolu74@gmail.com>
2023-11-16 16:06:54 +0100

    modified word explorer

--- app/word_explorer.py
+++ app/word_explorer.py
@@ -3,27 +3,8 @@ import os
 import json
 from dotenv import load_dotenv
 from typing import List, Dict
-from queryverse.llm import OpenAI
+from queryverse.llm import OpenAILLM
 from queryverse.prompter import SystemPrompter, UserPrompter
-
-
-def load_env_var(env_name: str = "OPENAI_API_KEY", 
-                env_path: str | None = None) -> str:
-    """
-        Load an environment variable from a .env file
-    Args:
-        env_name: The name of the environment variable
-        env_path: The path to the env file
-    
-    Returns:
-        str: value of the environment variable
-    """
-    if env_path and os.path.exists(env_path):
-        load_dotenv(env_path)
-    else:
-        load_dotenv()
-    env_var = os.getenv(env_name, "")
-    return env_var
 
 
 def json_parser(response: str) -> List[dict]:
@@ -35,76 +16,7 @@ def json_parser(response: str) -> List[dict]:
     return data
 
 
-def extract_json_in_string(input_string: str) -> str | None:
-    """
-    Extracts JSON from a given string.
-
-    Args:
-        input_string (str): The input string that may contain JSON data.
-
-    Returns:
-        str | None: If valid JSON is found in the input string, it is extracted,
-                    parsed, and returned as a string. If no JSON is found or if
-                    the JSON is invalid, None is returned.
-    """
-    pattern = r'\{[^{}]*\}'  # Pattern to detect JSON
-    match = re.search(pattern, input_string)
-    if match:
-        matched_text = match.group()
-        try:
-            # If a match is found, perform final verification with json.loads
-            # We do another json.dumps because we want a string output
-            json_string = json.dumps(json.loads(matched_text))
-        except json.JSONDecodeError:
-            json_string = None
-
-        return json_string
-    else:
-        return None
-
-
-def stream_json_examples(response):
-    """
-    Stream JSON Examples from a Generator of Byte Strings.
-
-    Args:
-        response (generator): A generator that yields byte strings.
-
-    Yields:
-        str: Valid JSON strings extracted from the accumulated chunks.
-    """
-    accumulated_chunk = ""
-    for chunk in response:
-        if 'assistant' not in chunk:
-            accumulated_chunk += chunk['no_role']
-
-        if '{' in accumulated_chunk and '}' in accumulated_chunk:
-            parsed_json = extract_json_in_string(accumulated_chunk)
-
-            if parsed_json:
-                yield parsed_json
-                accumulated_chunk = ""
-    else:
-        yield ""
-
-
-def stream_tokens(response):
-    """
-    Stream a token at a time.
-
-    Args:
-        response (generator): A generator that yields byte strings.
-
-    Yields:
-        str: stream a token at a time
-    """
-    accumulated_chunk = ""
-    for chunk in response:
-        if 'assistant' not in chunk:
-            yield chunk['no_role']
-
-
-def word_explainer(german_word: str, temperature: float) -> List[Dict[str, str]]:
+async def word_explainer(gpt: OpenAILLM, german_word: str, temperature: float) -> List[Dict[str, str]]:
     """
     Get an explanation and synonyms of a German word.
 
@@ -141,14 +53,14 @@ def word_explainer(german_word: str, temperature: float) -> List[Dict[str, str]]
     """)
 
     messages=[system_prompt(), user_prompt(word=german_word)]
-    response = OpenAI.prompt(messages, temperature, stream=False)
+    response = await gpt.aprompt(messages, temperature, stream=False)
     
     explanations_and_synonyms = json_parser(response['messages'][0]['assistant'])
     
     return explanations_and_synonyms
 
 
-def sentence_generator(german_word: str, number_of_sentences: int, temperature: float):
+async def sentence_generator(gpt: OpenAILLM, german_word: str, number_of_sentences: int, temperature: float):
     """
     Generate sentences in German based on a given word.
 
@@ -181,6 +93,6 @@ def sentence_generator(german_word: str, number_of_sentences: int, temperature: 
     """)
 
     messages=[system_prompt(), user_prompt(word=german_word, num_sent=number_of_sentences)]
-    response = OpenAI.prompt(messages, temperature, stream=True, max_tokens=3500)
+    response = await gpt.aprompt(messages, temperature, stream=True, max_tokens=3500)
     
     return response
